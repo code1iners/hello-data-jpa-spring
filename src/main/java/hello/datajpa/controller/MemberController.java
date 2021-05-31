4943f30d9cc88164c3fc366411058a4d3e2d7400
Codeliner <codeliner96@gmail.com>
2021-05-31 10:28:20 +0900

    Added how to use paging with Page interface.

--- src/main/java/hello/datajpa/controller/MemberController.java
+++ src/main/java/hello/datajpa/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.datajpa.controller;
 
+import hello.datajpa.dto.MemberDto;
 import hello.datajpa.entity.Member;
 import hello.datajpa.repository.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +23,6 @@ public class MemberController {
 
     @GetMapping("/members/{id}")
     public String findMember(@PathVariable("id") Long id) {
-        System.out.println("id = " + id);
         Member foundMember = memberRepository.findById(id).get();
         return foundMember.getUsername();
     }
@@ -33,8 +38,32 @@ public class MemberController {
         return member.getUsername();
     }
 
+    /**
+     * <h3>List as page.</h3>
+     * <p>find members as list with Page interface.</p>
+     * <p>Able to use query string.</p>
+     * <p>...?page=4&&size=3&&sort=id,desc</p>
+     * <p>Setting by application.xml or @PageableDefault annotation</p>
+     */
+    @GetMapping("/members")
+    public Page<MemberDto> listAsPage(
+//            @Qualifier("member")
+            @PageableDefault(size = 5) Pageable pageable
+//            , @Qualifier("order") Pageable orderPageable
+    ) {
+        // note. other way.
+//        PageRequest request = PageRequest.of(1, 2);
+//        Page<Member> members = memberRepository.findAll(request);
+
+        // note. basic way.
+        Page<Member> members = memberRepository.findAll(pageable);
+        return members.map(MemberDto::new);
+    }
+
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("member1"));
+        for (int i = 0; i < 100; i++) {
+            memberRepository.save(new Member("member" + i, i));
+        }
     }
 }
